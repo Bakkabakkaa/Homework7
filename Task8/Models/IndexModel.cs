a0941293cb609e5a73bc9dea8275360b82660b76
Art'm <kasko.artem45@gmail.com>
2026-01-06 01:53:28 +0300

    add Product and Index models

---
+++ Task8/Models/IndexModel.cs
@@ -0,0 +1,6 @@
+namespace Task8.Models;
+
+public class IndexModel
+{
+    public List<Product> Products { get; set; }
+}
---
+++ Task8/Models/Product.cs
@@ -0,0 +1,10 @@
+namespace Task8.Models;
+
+public class Product
+{
+    public int ID { get; set; }
+    public string Category { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+    public int Quantity { get; set; }
+}
